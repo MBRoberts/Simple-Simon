4456a20d14e42093d8720c25112356061f860c3e
Ben Roberts <mikebenroberts@gmail.com>
2016-08-21 22:26:05 -0500

    Added background music and video as well as set their playback rates to increase every 3 rounds. Also made the animation speed up every 3 rounds

--- public/js/simple-simon.js
+++ public/js/simple-simon.js
@@ -2,27 +2,35 @@
 
 $(document).ready(function() {
 
-    $('#video').off('play');
-    var buttonArray = [],
-        compareArray = [],
+    var music = document.getElementById("music"),
+        video = document.getElementById('background'),
+        sequenceArray = [],
+        sequenceArrayCopy = [],
         round = 1,
-        count = 0;
+        sequenceSpeed = 500;
+
+    //  Sets rate and volume of background video and music
+    music.playbackRate = .5;
+    music.volume = .25;
+    video.playbackRate = .5;
+
+        
 
     //  The light function simply lights the color passed in by the roundLoop function
-    function lightUp(color, delay) {   
+    function lightUp(btn, delay) {   
 
-        $(color).animate( {
-            opacity : .35,
+        $(btn).animate( {
+            opacity : 1,
             boxShadow : '0 0 30px #fff'
         }, delay). animate( {
-            opacity : 1,
+            opacity : .35,
             boxShadow : '10 10 10px #111'
         }, delay);
 
     }
 
     //  The uiClicks function saves the button the user clicks on
-    function uiClicks(compareArray) {
+    function uiClicks(sequenceCheckArray) {
 
         $('.box').on("click", function(e){
             
@@ -31,11 +39,11 @@ $(document).ready(function() {
 
             lightUp(buttonToLight, 100);  //  sends the color clicked on to be lit up by the lightUp function
              
-            if (buttonVal == compareArray[0]) {  //  Compares the button pressed with the first button in the array
+            if (buttonVal == sequenceCheckArray[0]) {  //  Compares the button pressed with the first button in the array
 
-                compareArray.shift();  //  deletes the first index
+                sequenceCheckArray.shift();  //  deletes the first index
 
-                if (compareArray.length == 0){  //  once the array is empty it shuts off the eventlistener and returns to the nextRandNum functoin 
+                if (sequenceCheckArray.length == 0){  //  once the array is empty it shuts off the eventlistener and returns to the nextRandNum functoin 
 
                     round++;
                     $(".box").off('click');
@@ -44,15 +52,16 @@ $(document).ready(function() {
                 } else {  //  if there are still elements in the array it deletes the eventlistener and recalls this function
 
                     $(".box").off('click');
-                    uiClicks(compareArray);
+                    uiClicks(sequenceCheckArray);
 
                 }
-            } else {  //  if the wrong key is entered the game is stopped
+            } else {  //  if the wrong key is entered the music is stopped and willie video plays then the page reloads
 
                 $(".box").off('click');
                 setTimeout(function(){
                     location.reload();
                 }, 6000)
+                music.pause();
                 $('#video').attr('src', 'https://www.youtube.com/embed/VDW0ZnZxjn4?rel=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0');
                 $('#video').css('display', 'block');
 
@@ -61,42 +70,52 @@ $(document).ready(function() {
     }
 
     // The roundLoop function will continue lighting up random colors then calling itself until the count has reached the round
-    function roundLoop(buttons) {
+    function roundLoop(sequence, count) {
         
         setTimeout(function() {
 
-            switch (buttons[count]) {   //  Lights up color according to the random number
+            switch (sequence[count]) {   //  Lights up color according to the random number
                 case 0 :
-                    lightUp('#red', 400);
+                    lightUp('#red', sequenceSpeed);
                     break;
                 case 1 :
-                    lightUp('#blue', 400);
+                    lightUp('#blue', sequenceSpeed);
                     break;
                 case 2 :
-                    lightUp('#green', 400);
+                    lightUp('#green', sequenceSpeed);
                     break;
                 case 3 :
-                    lightUp('#yellow', 400);
+                    lightUp('#yellow', sequenceSpeed);
                     break;
             }
             count++;
 
             if (count < round) {  //  Will continue running the function until the count reaches the round number
-                roundLoop(buttons);
+                roundLoop(sequence, count);
             } else {
-                uiClicks(buttons);  //  Once the sequence is complete it calls the UI function
+                uiClicks(sequence);  //  Once the sequence is complete it calls the UI function
             }
-        }, 1000)  //  Delays the loop by 1 sec
+        }, (sequenceSpeed * 2))  //  Delays the loop by 1 sec
     }
+
 
     function nextRandNum() {
 
-        count = 0;
+        if(round % 3 == 0) {  //  Increases the speed of the video, music, and animations
+            music.playbackRate += .5;
+            video.playbackRate += .75;
+            if(sequenceSpeed > 200) {
+                sequenceSpeed -= 100;
+            }
+        }
+
+        var count = 0;
         var randNum = Math.floor((Math.random() * 4));
-        buttonArray.push(randNum);  //  Pulls a random number between 0-3 and pushes it onto the button array
-        compareArray = buttonArray.slice(0);  //  copys the buttonArray so the array can be modified throughout the program without modifying the original sequence
-        $('#round').html("<h1>Round: " + round + "</h1>");  //  Displays the round number
-        roundLoop(compareArray);    //  Starts light sequence
+        sequenceArray.push(randNum);  //  Pulls a random number between 0-3 and pushes it onto the button array
+        var sequenceArrayCopy = sequenceArray.slice(0);  //  copys the sequenceArray so the array can be modified throughout the program without modifying the original sequence
+        console.log(sequenceArray);
+        $('#round').html("Round: " + round);  //  Displays the round number
+        roundLoop(sequenceArrayCopy, count);    //  Starts light sequence
 
     }
     
